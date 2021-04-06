04400eaf4d104440ee21db5bf08029273b918c89
Anish <anish@gmail.com>
2021-04-06 12:04:37 +0530

    Comitting the changes made to LoginTest

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -3,6 +3,7 @@ package testcases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,5 +17,9 @@ public class LoginTest {
 		 WebDriver driver = new ChromeDriver();
 		 driver.navigate().to("https://www.google.com/");
 		 
+		 
+		String ActualTitle = driver.getTitle();
+		 String ExpectedTitle = "Google";
+		 Assert.assertEquals(ActualTitle, ExpectedTitle);
 	}
 }
